7ce0d78c71d0e71d9b78c849e3f3511b6c33904d
ryan <ryantxu1@gmail.com>
2021-10-11 11:20:54 -0500

    NPCS done

--- d20/NPCS/HashNPC.py
+++ d20/NPCS/HashNPC.py
@@ -11,6 +11,7 @@ from typing import Union, TYPE_CHECKING
 if TYPE_CHECKING:
     from d20.Manual.Facts import Fact
 
+
 # Process basic information to initially populate fact table
 # Compute Hashes
 
