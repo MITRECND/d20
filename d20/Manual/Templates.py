366980e1b5efbeba1cb0dbaefc28a4912f5af1ef
ryan <ryantxu1@gmail.com>
2021-11-02 08:45:14 -0500

    Template done

--- d20/Manual/Templates.py
+++ d20/Manual/Templates.py
@@ -4,16 +4,25 @@ from d20.Screens import loadScreen
 from d20.BackStories import loadBackStory
 from d20.Manual.Logger import logging
 
-LOGGER = logging.getLogger(__name__)
+from typing import Optional, Dict, List, Type, Callable
+from d20.Manual.Logger import Logger
+from d20.NPCS import Tnpc
+from d20.BackStories import Tbackstory
+from d20.Players import Tplayer
+from d20.Screens import Tscreen
+from d20.Manual.Console import PlayerConsole, NPCConsole, BackStoryConsole
 
 
-def registerNPC(*args, **kwargs):
+LOGGER: Logger = logging.getLogger(__name__)
+
+
+def registerNPC(*args, **kwargs: str) -> Callable[..., Type[Tnpc]]:
     """A decorator for registering an NPC
 
         This decorator expects keyword arguments that match up with the
         RegistrationForm class from d20.Manual.Registration
     """
-    def _registerNPC(cls):
+    def _registerNPC(cls: Type[Tnpc]) -> Type[Tnpc]:
         LOGGER.debug("Registering NPC %s"
                      % (cls.__qualname__))
         loadNPC(cls, **kwargs)
@@ -21,13 +30,13 @@ def registerNPC(*args, **kwargs):
     return _registerNPC
 
 
-def registerBackStory(*args, **kwargs):
+def registerBackStory(*args, **kwargs: str) -> Callable[..., Type[Tbackstory]]:
     """A decorator for registering an BackStory
 
         This decorator expects keyword arguments that match up with the
         BackStoryRegistrationForm class from d20.Manual.Registration
     """
-    def _registerBackStory(cls):
+    def _registerBackStory(cls: Type[Tbackstory]) -> Type[Tbackstory]:
         LOGGER.debug("Registering BackStory %s"
                      % (cls.__qualname__))
         loadBackStory(cls, **kwargs)
@@ -35,13 +44,13 @@ def registerBackStory(*args, **kwargs):
     return _registerBackStory
 
 
-def registerPlayer(*args, **kwargs):
+def registerPlayer(*args, **kwargs: str) -> Callable[..., Type[Tplayer]]:
     """A decorator for registering a Player
 
         This decorator expects keyword arguments that match up with the
         RegistrationForm class from d20.Manual.Registration
     """
-    def _registerPlayer(cls):
+    def _registerPlayer(cls: Type[Tplayer]) -> Type[Tplayer]:
         LOGGER.debug("Registering Player %s"
                      % (cls.__qualname__))
         loadPlayer(cls, **kwargs)
@@ -49,13 +58,13 @@ def registerPlayer(*args, **kwargs):
     return _registerPlayer
 
 
-def registerScreen(*args, **kwargs):
+def registerScreen(*args, **kwargs: str) -> Callable[..., Type[Tscreen]]:
     """A decorator for registering a Screen
 
         This decorator expects kwargs which cooresponds to the
         ScreenRegistrationForm class
     """
-    def _registerScreen(cls):
+    def _registerScreen(cls: Type[Tscreen]) -> Type[Tscreen]:
         LOGGER.debug("Registering Screen %s"
                      % (cls.__qualname__))
         loadScreen(cls, **kwargs)
@@ -69,7 +78,7 @@ class PlayerTemplate:
         This class does some basic high-level common functions required by
         the framework to work
     """
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         """PlayerTemplate __init__ function
 
             Please ensure you call this class's init function in your Player
@@ -77,13 +86,13 @@ class PlayerTemplate:
         if 'console' not in kwargs:
             raise RuntimeError("Player Console Interface not passed in "
                                "to init of Player")
-        self.console = kwargs['console']
+        self.console: PlayerConsole = kwargs['console']
         if 'options' in kwargs:
-            self.options = kwargs['options']
+            self.options: Dict = kwargs['options']
         else:
             self.options = dict()
 
-    def handleFact(self, **kwargs):
+    def handleFact(self, **kwargs) -> None:
         """Function that is called to handle a 'fact'
 
             When the framework is presented with a fact that a player has
@@ -92,7 +101,7 @@ class PlayerTemplate:
         """
         raise NotImplementedError("Player Implementation Required")
 
-    def handleHypothesis(self, **kwargs):
+    def handleHypothesis(self, **kwargs) -> None:
         """Function that is called to handle a 'hypothesis'
 
             When the framework is presented with a hypothesis that a player has
@@ -101,14 +110,14 @@ class PlayerTemplate:
         """
         raise NotImplementedError("Player Implementation Required")
 
-    def saveState(self, **kwargs):
+    def saveState(self, **kwargs) -> None:
         """Function that is called to enable a player instance to save state
 
             TBD
         """
         raise NotImplementedError("Player implementation Required")
 
-    def loadState(self, **kwargs):
+    def loadState(self, **kwargs) -> None:
         """Function that is called to load saved state information
 
             TBD
@@ -122,7 +131,7 @@ class NPCTemplate:
         This class does some basic high-level common functions required by
         the framework to work
     """
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         """NPCTemplate __init__ function
 
             Please ensure you call this class's init function in your NPC
@@ -130,13 +139,13 @@ class NPCTemplate:
         if 'console' not in kwargs:
             raise RuntimeError("NPC Console Interface not passed in "
                                "to init of NPC")
-        self.console = kwargs['console']
+        self.console: NPCConsole = kwargs['console']
         if 'options' in kwargs:
-            self.options = kwargs['options']
+            self.options: Dict = kwargs['options']
         else:
             self.options = dict()
 
-    def handleData(self, **kwargs):
+    def handleData(self, **kwargs) -> None:
         """Function called when new object seen
 
             When the framework is presented with a new object it is
@@ -151,7 +160,7 @@ class BackStoryTemplate:
         This class does some basic high-level common functions required by
         the framework to work
     """
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs) -> None:
         """BackStoryTemplate __init__ function
 
             Please ensure you call this class's init function in your BackStory
@@ -159,13 +168,13 @@ class BackStoryTemplate:
         if 'console' not in kwargs:
             raise RuntimeError("BackStory Console Interface not passed in "
                                "to init of BackStory")
-        self.console = kwargs['console']
+        self.console: BackStoryConsole = kwargs['console']
         if 'options' in kwargs:
-            self.options = kwargs['options']
+            self.options: Dict = kwargs['options']
         else:
             self.options = dict()
 
-    def handleFact(self, **kwargs):
+    def handleFact(self, **kwargs) -> None:
         """Function that is called to handle a 'fact'
 
             When the framework is presented with a fact that a backstory has
@@ -181,10 +190,10 @@ class ScreenTemplate:
         some basic setup needed by the framework
     """
     def __init__(self, **kwargs):
-        self.objects = None
-        self.facts = None
-        self.hyps = None
-        self.options = dict()
+        self.objects: Optional[List] = None
+        self.facts: Optional[Dict] = None
+        self.hyps: Optional[Dict] = None
+        self.options: Dict = dict()
 
         for (key, value) in kwargs.items():
             if key == 'objects':
@@ -205,7 +214,7 @@ class ScreenTemplate:
         if self.hyps is None:
             raise RuntimeError("'hyps' must be supplied in kwargs")
 
-    def filter(self):
+    def filter(self) -> None:
         """filter is meant to be used to provide a trimmed down view of data
         based on configuration provided by the user. This function should be
         called the present function.
@@ -217,7 +226,7 @@ class ScreenTemplate:
         """
         raise NotImplementedError("This method must be overriden by children")
 
-    def present(self):
+    def present(self) -> None:
         """present is meant to provide a printable string for user consumption.
         By convention it should call the filter function (above) before
         returning anything
